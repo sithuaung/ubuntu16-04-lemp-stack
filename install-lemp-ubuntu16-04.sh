1e067901ab5bc8d188e4eb302f1b0b615ae9e457
webcrazy <cosmos.stu@gmail.com>
2016-07-19 20:01:06 +0630

    updated lemp shell

--- install-lemp-ubuntu16-04.sh
+++ install-lemp-ubuntu16-04.sh
@@ -5,13 +5,13 @@ echo " --- Updating packages Lists --- "
 sudo apt-get update
 
 echo " --- Installing php-7 --- "
-sudo apt install php7.0-cli
+sudo apt install -y php7.0-cli
 
 echo " --- Installing nginx --- "
-sudo apt-get install nginx
+sudo apt-get install -y nginx
 
 echo " --- Installing mysql --- "
-sudo apt install mysql-server mysql-client
+sudo apt install -y mysql-server mysql-client
 
 echo " --- Installing composer --- "
 sudo curl -sS https://getcomposer.org/installer | php
@@ -19,9 +19,9 @@ sudo mv composer.phar /usr/local/bin/composer
 
 
 echo " --- Installing necessary extensions for laravel --- "
-sudo apt-get install php7.0-mbstring
-sudo apt-get install php-xml
-sudo apt-get install php7.0-zip
-sudo apt-get install php7.0-fpm
+sudo apt-get install -y php7.0-mbstring
+sudo apt-get install -y php-xml
+sudo apt-get install -y php7.0-zip
+sudo apt-get install -y php7.0-fpm
 
 echo " --- Enjoy your LEMP --- "
