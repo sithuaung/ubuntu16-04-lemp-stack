fdc4de9694bf18c4a281a1fe02c69c7bf2cbc3fb
webcrazy <cosmos.stu@gmail.com>
2017-12-02 12:39:53 +0630

    updated with 7.2

--- lemp-for-ubuntu.sh
+++ lemp-for-ubuntu.sh
@@ -11,14 +11,14 @@ sudo apt-get install -y software-properties-common python-software-properties
 echo " --- Installing php-7 --- "
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y php7.1-fpm
+sudo apt-get install -y php7.2-fpm
 
 echo " --- Installing necessary php extensions for laravel --- "
-sudo apt-get install -y php7.1-mbstring
-sudo apt-get install -y php7.1-xml
-sudo apt-get install -y php7.1-zip
-sudo apt-get install -y php7.1-mysql
-sudo apt-get install -y php7.1-curl
+sudo apt-get install -y php7.2-mbstring
+sudo apt-get install -y php7.2-xml
+sudo apt-get install -y php7.2-zip
+sudo apt-get install -y php7.2-mysql
+sudo apt-get install -y php7.2-curl
 
 echo " --- Installing nginx --- "
 sudo apt-get install -y nginx
