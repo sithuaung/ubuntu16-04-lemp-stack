8d33d634be4e3b56debbcc03e9609c22f4623a43
Si Thu Aung <cosmos.stu@gmail.com>
2019-03-15 09:37:25 +0630

    updated 7.3

--- lemp-for-ubuntu.sh
+++ lemp-for-ubuntu.sh
@@ -11,14 +11,14 @@ sudo apt-get install -y software-properties-common python-software-properties
 echo " --- Installing php-7 --- "
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-sudo apt-get install -y php7.2-fpm
+sudo apt-get install -y php7.3-fpm
 
 echo " --- Installing necessary php extensions for laravel --- "
-sudo apt-get install -y php7.2-mbstring
-sudo apt-get install -y php7.2-xml
-sudo apt-get install -y php7.2-zip
-sudo apt-get install -y php7.2-mysql
-sudo apt-get install -y php7.2-curl
+sudo apt-get install -y php7.3-mbstring
+sudo apt-get install -y php7.3-xml
+sudo apt-get install -y php7.3-zip
+sudo apt-get install -y php7.3-mysql
+sudo apt-get install -y php7.3-curl
 
 echo " --- Installing nginx --- "
 sudo apt-get install -y nginx
